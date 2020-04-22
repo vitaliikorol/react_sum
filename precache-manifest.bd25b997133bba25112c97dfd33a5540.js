f1cc4a31b40109e71343a967634e3c31cb631ec8
Vitalii Korol <vitaliikorol@github.com>
2020-04-22 18:00:28 +0300

    Updates

---
+++ precache-manifest.bd25b997133bba25112c97dfd33a5540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f241dcb0db6a7eb40c3021ff1553f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75550b8aa5674a2b3c0e",
+    "url": "./static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7f28560cd54ef99d9bd1",
+    "url": "./static/js/2.e52bef64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e52bef64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75550b8aa5674a2b3c0e",
+    "url": "./static/js/main.8f7492bc.chunk.js"
+  },
+  {
+    "revision": "8f752b305088beda2000",
+    "url": "./static/js/runtime-main.9f2fd0c6.js"
+  }
+]);
